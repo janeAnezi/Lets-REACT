13d1a17f406188377dbbc2745122113f57a55ac1
Anezijane <ahnnieanezi@gmail.com>
2024-10-05 21:41:18 +0100

    create nav component

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return ( 
+        <>
+            <div className="navig">
+                <h1>pAbV Kiddies</h1>
+
+                <div className="links">
+                    <a href="#home">Home</a>
+                    <a href="/create">Create Post</a>
+                </div>
+            </div>
+
+        </>
+     );
+}
+ 
+export default NavBar;
