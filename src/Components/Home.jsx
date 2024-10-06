2f0987a8e9c96b7716fddc9a6ad65a05d0dd55b1
Anezijane <ahnnieanezi@gmail.com>
2024-10-06 18:05:15 +0100

    reuse Bloglist component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,9 +6,9 @@ import BlogList from "./BlogList";
 
 const Home = () => {
    const [blogs, setBlogs] = useState([
-    { id: 1, image: {kid01}, title: "Blog 1", content: "This is the content of the first blog", author: 'Anezi' },
-    { id: 2, image: {kid02}, title: "Blog 2", content: "This is the...", author: "Adiel" },
-    { id: 3, image: {kid03}, title: "Blog 3", content: "This is the content of the third blog...", author: "pabv" }
+    { id: 1, image: kid01, title: "Blog 1", content: "This is the content of the first blog", author: 'Anezi' },
+    { id: 2, image: kid02, title: "Blog 2", content: "This is the...", author: "Adiel" },
+    { id: 3, image: kid03, title: "Blog 3", content: "This is the content of the third blog...", author: "pabv" }
    ])
    
 
@@ -16,6 +16,7 @@ const Home = () => {
     return ( <>
         <div className="home">
             <BlogList blogs={blogs} title="Designs and Styles"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Adiel' )} title="Trending" />
 
         </div>
         
