37cccb5f82a2a4d5a1ebd27ffb6bc8b2a0b567d4
Anezijane <ahnnieanezi@gmail.com>
2024-10-06 09:03:21 +0100

    add click events and functon

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,16 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Button clicked');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log(`${name}, You clicked the Button again`);
+    }
     return ( <>
         <h1 className="home"> This is the body of the post</h1>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => {handleClickAgain('Jane')}}>Click Again</button>
     </> );
 }
  
