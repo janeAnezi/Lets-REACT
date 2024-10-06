25bec7734c7bbbe29625fe91f320cfb2f1d97f88
Anezijane <ahnnieanezi@gmail.com>
2024-10-06 09:55:09 +0100

    add kidies images

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import kid01 from '../assets/images/kidImg01.jpg'
+import kid02 from '../assets/images/kidImg02.jpg'
+import kid03 from '../assets/images/kidImg03.jpg'
 
 const Home = () => {
-   const [name, setName] = useState('Jane')
-    const handleClick = () => {
-       setName('Anezi');
-    }
+   const [blogs, setBlogs] = useState([
+    { id: 1, url: {kid01}, title: "Blog 1", content: "This is the content of the first blog", author: 'Anezi' },
+    { id: 2, url: {kid02}, title: "Blog 2", content: "This is the...", author: "Adiel" },
+    { id: 3, url: {kid03}, title: "Blog 3", content: "This is the content of the third blog...", author: "pabv" }
+   ])
+   
 
    
     return ( <>
